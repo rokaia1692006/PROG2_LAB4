b56040525430d8b0efc4aeb00fbf4bd3cebcb5be
nour <elashwahnour@gmail.com>
2025-10-18 22:26:07 +0300

    Creating the record interface

---
+++ src/prog2_lab4/Record.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package prog2_lab4;
+
+/**
+ *
+ * @author LapTop
+ */
+public interface Record {
+    public String getSearchKey();
+    public String lineRepresentation();
+}
